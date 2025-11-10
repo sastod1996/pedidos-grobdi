6ef4fa19c476ef06fda7fea0caf491dc58e9cef9
GrobdiDevOwO <programador.grobdi@gmail.com>
2025-11-10 16:29:11 -0500

    fix: se cambio la ruta del job

--- app/Console/Commands/DispatchMigracionMuestrasCommand.php
+++ app/Console/Commands/DispatchMigracionMuestrasCommand.php
@@ -2,7 +2,7 @@
 
 namespace App\Console\Commands;
 
-use App\Jobs\MigrateMuestrasEstadosJob;
+use App\Infrastructure\Jobs\MigrateMuestrasEstadosJob;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
 
