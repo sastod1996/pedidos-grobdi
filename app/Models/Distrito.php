46b195c8d3b9845fa7f513360381f138750aaa8c
R0dri-Dev <u22232910@utp.edu.pe>
2025-11-15 10:52:23 -0500

    style: remove unnecessary whitespace in Distrito model methods

--- app/Models/Distrito.php
+++ app/Models/Distrito.php
@@ -8,15 +8,15 @@ class Distrito extends Model
 {
     public function provincia()
     {
-        return $this->belongsTo(Provincia::class); 
+        return $this->belongsTo(Provincia::class);
     }
     public function doctores()
     {
-        return $this->hasMany(Doctor::class); 
+        return $this->hasMany(Doctor::class);
     }
     public function listas()
     {
-        return $this->belongsToMany(Lista::class, 'lista_distrito'); 
+        return $this->belongsToMany(Lista::class, 'lista_distrito');
     }
     public $timestamps = false;
 }
