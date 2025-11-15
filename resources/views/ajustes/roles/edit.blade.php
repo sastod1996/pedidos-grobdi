960298babcd3254bc8fc4043853c9b1c2242e853
R0dri-Dev <u22232910@utp.edu.pe>
2025-11-15 10:53:12 -0500

    feat: refactor role edit form to use Grobdi input components for improved consistency and readability

--- resources/views/ajustes/roles/edit.blade.php
+++ resources/views/ajustes/roles/edit.blade.php
@@ -21,18 +21,19 @@
             </ul>
         </div>
     @endif
-    <form action="{{ route('roles.update', $role) }}" method="POST">
+    <form action="{{ route('roles.update', $role) }}" method="POST" class="grobdi-form">
         @csrf @method('PUT')
-        <div class="form-group">
-            <div class="form-group">
-                <label class="grobdi-label">Nombre</label>
-                <input type="text" name="name" value="{{ $role->name }}" class="form-control grobdi-input" required>
-            </div>
-            <div class="form-group">
-                <label class="grobdi-label">Descripcion</label>
-                <input type="text" name="description" value="{{ $role->description }}" class="form-control grobdi-input">
-            </div>
-        </div>
+        <x-grobdi.form.input
+            label="Nombre"
+            name="name"
+            :value="old('name', $role->name)"
+            required
+        />
+        <x-grobdi.form.input
+            label="Descripcion"
+            name="description"
+            :value="old('description', $role->description)"
+        />
         <button class="btn btn-grobdi btn-primary-grobdi">Actualizar</button>
         <a href="{{ route('roles.index') }}" class="btn btn-secondary">Cancelar</a>
     </form>
